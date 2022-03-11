7360e10d1686d6b73a2ac867a73d99e9c248480e
Khanhhe151416 <khanhdvhe151416@fpt.edu.vn>
2022-03-11 10:26:22 +0700

    commit

---
+++ AssignmentPRJ301SP22/src/java/model/FoodBill.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author doan7
+ */
+public class FoodBill {
+    private food food;
+    private bill bill;
+
+    public food getFood() {
+        return food;
+    }
+
+    public void setFood(food food) {
+        this.food = food;
+    }
+
+    public bill getBill() {
+        return bill;
+    }
+
+    public void setBill(bill bill) {
+        this.bill = bill;
+    }
+    
+    
+    
+}
--- AssignmentPRJ301SP22/src/java/model/bill.java
+++ AssignmentPRJ301SP22/src/java/model/bill.java
@@ -5,10 +5,80 @@
  */
 package model;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author doan7
  */
 public class bill {
+    private int id;
+    private String name;
+    private Date dateBill;
+    private float totalPayment;
+    private table table;
+    private staff staff;
+    
+    private List<FoodBill> foodBill = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateBill() {
+        return dateBill;
+    }
+
+    public void setDateBill(Date dateBill) {
+        this.dateBill = dateBill;
+    }
+
+    public float getTotalPayment() {
+        return totalPayment;
+    }
+
+    public void setTotalPayment(float totalPayment) {
+        this.totalPayment = totalPayment;
+    }
+
+    public table getTable() {
+        return table;
+    }
+
+    public void setTable(table table) {
+        this.table = table;
+    }
+
+    public staff getStaff() {
+        return staff;
+    }
+
+    public void setStaff(staff staff) {
+        this.staff = staff;
+    }
+
+    public List<FoodBill> getFoodBill() {
+        return foodBill;
+    }
+
+    public void setFoodBill(List<FoodBill> foodBill) {
+        this.foodBill = foodBill;
+    }
+    
+    
     
 }
