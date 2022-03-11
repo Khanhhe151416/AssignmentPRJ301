ff94bc8670c95b362bacd766d037c1ca7e610300
Khanhhe151416 <khanhdvhe151416@fpt.edu.vn>
2022-03-11 10:13:53 +0700

    commit

---
+++ AssignmentPRJ301SP22/src/java/model/staff.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author doan7
+ */
+public class staff {
+    private int id;
+    private String name;
+    private float salary;
+    private account account;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public account getAccount() {
+        return account;
+    }
+
+    public void setAccount(account account) {
+        this.account = account;
+    }
+    
+    
+}
